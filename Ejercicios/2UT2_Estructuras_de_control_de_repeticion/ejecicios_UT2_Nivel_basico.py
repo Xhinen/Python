0e49f9322c82c0b58caddaa240ebd39b051fa11c
Xhinen <xhinen@proton.me>
2025-05-08 10:28:45 +0200

    actualización nombres archivos

---
+++ Ejercicios/2UT2_Estructuras_de_control_de_repeticion/ejecicios_UT2_Nivel_basico.py
@@ -0,0 +1,4 @@
+# Ejercicio 11
+# Escribe un programa que pida al usuario dos números enteros e imprima la secuencia de números entre ellos (incluidos) en orden ascendente. 
+# El primer número siempre será menor que el segundo.
+
