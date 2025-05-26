26235463f307f753200ac6ff55cb792ee4c205e1
Xhinen <xhinen@proton.me>
2025-05-26 13:41:05 +0200

    ACTUALIZACIÓN NOMBRE DE DIRECTORIOS

--- Ejercicios/2UT2_Estructuras_de_control_FOR/ejecicios_UT2_Nivel_basico.py
+++ Ejercicios/2UT2_Estructuras_de_control_FOR/ejecicios_UT2_Nivel_basico.py
@@ -113,7 +113,3 @@ while num != -1:
 # Escribe un programa que dado una serie de notas introducidas por el usuario, hasta que introduzca un -1,
 # imprima el número de notas correctas introducidas, la media de las notas y cuantas de estas notas son 10.
 # El programa debe imprimir la media de todas las notas introducidas, menos el -1.
-
-nota = int(input("Introduce la nota: "))
-
-while nota != -1:
