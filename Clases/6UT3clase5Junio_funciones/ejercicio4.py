4ac7e71ec8e9ba60aa204873118d2bba992f2da3
Xhinen <xhinen@proton.me>
2025-06-05 17:09:44 +0200

    clase 5 de junio

---
+++ Clases/6UT3clase5Junio_funciones/ejercicio4.py
@@ -0,0 +1,8 @@
+# Ejercicio 4 - Verificar si un número es primo
+# Escribe un programa que pida al usuario un número entero y verifique si es primo. 
+# El programa debe definir una función que reciba el número, realice la verificación 
+# y luego imprima si el número es primo o no.
+
+
+def es_primo(numero):
+    # Un número es primo si solo es divisible entre 1 y si mismo.
