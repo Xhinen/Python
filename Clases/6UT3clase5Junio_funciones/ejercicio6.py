1b54ae65094031abc3557f4541ba8419cc12cacd
Xhinen <xhinen@proton.me>
2025-06-05 17:58:34 +0200

    actualizacion clase 5 de junio

---
+++ Clases/6UT3clase5Junio_funciones/ejercicio6.py
@@ -0,0 +1,8 @@
+# Ejercicio 6 - Convierte string 
+# Recibe un string y lo convierte a mayúsculas
+
+def convierte_string(frase):
+    frase = frase.upper()
+
+frase = input("Introduce una frase que quieras capitalizar\n")
+print(convierte_string(frase))
